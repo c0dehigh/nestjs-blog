391e9ba8fea841b2e24fc9bd419a471c8f0fc41c
Hamed zibandeh <high_dev@outlook.com>
2026-04-19 12:12:08 +0330

    refactor: standardize formatting in configuration files

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -4,7 +4,7 @@ export default registerAs('database', () => ({
   host: process.env.DATABASE_HOST || 'localhost',
   port: process.env.DATABASE_PORT ? parseInt(process.env.DATABASE_PORT) : 5432,
   user: process.env.DATABASE_USER || 'postgres',
-  password: process.env.DATABASE_PASSWORD || '1234',
+  password: process.env.DATABASE_PASSWORD || 'root',
   name: process.env.DATABASE_NAME || 'nestjs-blog',
   synchronize: process.env.DATABASE_SYNC === 'true' ? true : false,
   autoLoadEntities: process.env.DATABASE_AUTOLOAD === 'true' ? true : false,
