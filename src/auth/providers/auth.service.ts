7c9066231d57020b5e0b9af5e9039fd67b0dd4f6
Hamed zibandeh <hamedzibandeh70@gmail.com>
2026-05-06 15:05:00 +0330

    feat: inject RefreshTokensProvider and implement refreshTokens method

--- src/auth/providers/auth.service.ts
+++ src/auth/providers/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable, Inject, forwardRef } from '@nestjs/common';
 import { UsersService } from 'src/users/providers/users.service';
 import { SignInDto } from '../dtos/signin.dto';
 import { SignInProvider } from './sign-in.provider';
+import { RefreshTokenDto } from '../dtos/refresh-token.dto';
+import { RefreshTokensProvider } from './refresh-tokens.provider';
 
 @Injectable()
 export class AuthService {
@@ -18,13 +20,18 @@ export class AuthService {
      */
 
     private readonly signInProvider: SignInProvider,
+
+    /**
+     * Inject refreshTokensProvider
+     */
+    private readonly refreshTokensProvider: RefreshTokensProvider,
   ) {}
 
   public async signIn(signInDto: SignInDto) {
     return await this.signInProvider.signIn(signInDto);
   }
 
-  public isAuth() {
-    return true;
+  public async refreshTokens(refreshTokenDto: RefreshTokenDto) {
+    return this.refreshTokensProvider.refreshTokens(refreshTokenDto);
   }
 }
