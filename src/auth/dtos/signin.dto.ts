e488ec714c74fff212c76ebec56a36ab9a9c2fa4
Hamed zibandeh <high_dev@outlook.com>
2026-05-07 10:09:51 +0330

    feat: update DTOs to use definite assignment assertions for refreshToken, email, and password

--- src/auth/dtos/signin.dto.ts
+++ src/auth/dtos/signin.dto.ts
@@ -3,8 +3,8 @@ import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 export class SignInDto {
   @IsEmail()
   @IsNotEmpty()
-  email: string;
+  email!: string;
   @IsString()
   @IsNotEmpty()
-  password: string;
+  password!: string;
 }
