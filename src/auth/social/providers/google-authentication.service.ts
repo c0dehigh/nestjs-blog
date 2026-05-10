1bce76b8f7a3f85a01581db9869c394768dab08a
Hamed Zibandeh <86211341+c0dehigh@users.noreply.github.com>
2026-05-10 09:04:10 +0330

    feat: implement GoogleAuthenticationService with OAuth2 client initialization and authentication method

--- src/auth/social/providers/google-authentication.service.ts
+++ src/auth/social/providers/google-authentication.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import type { ConfigType } from '@nestjs/config';
+import { OAuth2Client } from 'google-auth-library';
+import jwtConfig from 'src/auth/config/jwt.config';
+import { GoogleTokenDto } from '../dtos/google-token.dto';
 
 @Injectable()
-export class GoogleAuthenticationService {}
+export class GoogleAuthenticationService implements OnModuleInit {
+  private oAuthClinet: OAuth2Client;
+
+  constructor(
+    /**
+     * Inject jwtConfiguration
+     */
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+  ) {}
+
+  onModuleInit() {
+    const clientId = this.jwtConfiguration.googleClientId;
+    const clientSecret = this.jwtConfiguration.googleClientSecret;
+
+    this.oAuthClinet = new OAuth2Client(clientId, clientSecret);
+  }
+
+  public async authentication(googleTokenDto: GoogleTokenDto) {
+    // verify the token send by user
+    // extract payload from google jwt
+    // find the user in database from googleId
+    // if googleId exist in database generate token
+    // if not create user then generate token
+    // throw anuthentication exception
+  }
+}
