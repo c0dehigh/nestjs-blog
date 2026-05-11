70aaf824e51f61b0b5afa3ed05b76a3fc0fda420
Hamed Zibandeh <86211341+c0dehigh@users.noreply.github.com>
2026-05-11 10:40:50 +0330

    Add global DataResponseInterceptor
    Import DataResponseInterceptor in main.ts and register it with app.useGlobalInterceptors(new DataResponseInterceptor()). This makes the interceptor apply globally to standardize/wrap API responses across the application.

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { DataResponseInterceptor } from './common/interceptors/data-response/data-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -32,6 +33,11 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
   // Enable Cors
   app.enableCors();
+
+  // Add Global Intercepter
+
+  app.useGlobalInterceptors(new DataResponseInterceptor());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
