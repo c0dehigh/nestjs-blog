04e0a9c76d2aca3667b2ae7b4691fbab9ba8f976
Hamed Zibandeh <86211341+c0dehigh@users.noreply.github.com>
2026-05-11 11:27:02 +0330

    Wrap responses with apiVersion via ConfigService
    Inject ConfigService into DataResponseInterceptor and replace console logging with a response transform. The interceptor now uses rxjs map to wrap handler results into an object { apiVersion: configService.get('appConfig.apiVersion'), data } and imports map. This standardizes outgoing responses to include the API version.

--- src/common/interceptors/data-response/data-response.interceptor.ts
+++ src/common/interceptors/data-response/data-response.interceptor.ts
@@ -4,13 +4,18 @@ import {
   Injectable,
   NestInterceptor,
 } from '@nestjs/common';
-import { Observable, tap } from 'rxjs';
+import { ConfigService } from '@nestjs/config';
+import { Observable, tap, map } from 'rxjs';
 
 @Injectable()
 export class DataResponseInterceptor implements NestInterceptor {
+  constructor(private readonly configService: ConfigService) {}
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    console.log('Before...');
-
-    return next.handle().pipe(tap((data) => console.log('After...', data)));
+    return next.handle().pipe(
+      map((data) => ({
+        apiVersion: this.configService.get('appConfig.apiVersion'),
+        data: data,
+      })),
+    );
   }
 }
